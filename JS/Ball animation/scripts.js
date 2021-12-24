edb0cc5a3acaac04c58663e057b6830dbf64ae3b
tamakhusunder <tamakhusunder@gmail.com>
2021-12-24 15:42:05 +0545

    js: ball animation updated

--- JS/Ball animation/scripts.js
+++ JS/Ball animation/scripts.js
@@ -1,44 +1,16 @@
 function createContainer(h,w){
-    var container = document.createElement("div");
+    let container = document.createElement("div");
     container.style.height = h + "px";
     container.style.width = w + 'px';
     container.style.border = '1px solid black';
+    container.style.position = 'relative';
     document.body.appendChild(container);
-    
-    let x=230, y=10 ,speed=5;
-    setInterval(()=>{
-        animationBall(x,y,container);
-        if(y>=490){
-            speed = -speed;
-        }
-        else if (y<=9) {
-            speed=-speed;
-        }
-        y+=speed;
-        console.log(y);
-    },10);
+
+    return container;
 
 }
 
 function animationBall(x,y,container){
-    const point = document.createElement("div");
-    point.style.width = "20px";
-    point.style.height = "20px";
-    point.style.position = "absolute";
-    point.style.top = y + "px";
-    point.style.left = x + "px";
-    point.style.background = "red";
-    point.style.borderRadius = "50%";
-    // console.log(point);
-    container.appendChild(point);
-    setTimeout(()=>{
-        container.removeChild(point);
-
-    },10)
-
-}
-
-function scatterPoint(x,y,container){
     const point = document.createElement("div");
     point.style.width = "50px";
     point.style.height = "50px";
@@ -47,16 +19,30 @@ function scatterPoint(x,y,container){
     point.style.left = x + "px";
     point.style.background = "red";
     point.style.borderRadius = "50%";
-    // console.log(point);
     container.appendChild(point);
 
+    setTimeout(()=>{
+        container.removeChild(point);
+    },10)
+
 }
 
 
 
-function render(){
-    let container = createContainer(500,500);
-}
+let x=300, y=0, speed=5, sign=1;
+let container = createContainer(600,600);
+
+setInterval(()=>{
+    animationBall(x,y,container);
+    y = y + speed*sign;
+    if(y>=550){
+        sign = -1;
+    }
+    else if (y<=0) {
+        sign = +1
+    }
+    console.log(y);
+},10);
 
 
-render();
+
