a3a98e53c6a11190c3918be63295ad21c1b87a18
tamakhusunder <tamakhusunder@gmail.com>
2021-12-26 13:52:54 +0545

    js: assignment1 carousel completed

--- JS/Assignment 1-Image Carousel/src/carousel.js
+++ JS/Assignment 1-Image Carousel/src/carousel.js
@@ -1,71 +1,212 @@
-var container = document.getElementsByClassName('carousel-container')[0];
-var wrapperImage =  document.querySelector('.carousel-image-wrapper');
 
-const imageWidth = 600;
-const imageCount = wrapperImage.children.length;
-console.log(imageCount)
-
-// images.style.width = `${imageCount * imageWidth}px`;
-//loop for horizontal alignment of image
-for (let i = 0; i < imageCount; i++) {
-    const image = wrapperImage.children[i];
-    image.style.left = `${i * imageWidth}px`;
-}
-
-const prevBtn = document.getElementById("prev");
-const nextBtn = document.getElementById("next");
-
-let currentIndex=0;
-let velocity = 10;
-let interval;
-let currentImagePosition;
-let newImagePosition;
-let dx;
-console.log("---->>>",currentIndex);
-nextBtn.onclick = function () {
-    if (currentIndex==2){
-        currentIndex=0;
-    }
-    else{
-        currentIndex++;
-    }
-    console.log("//////",currentIndex);
-    for (let i = 0; i < imageCount; i++) {
-        const image = wrapperImage.children[i];
-        currentImagePosition = parseInt(image.style.left);
-        if (currentIndex == 2){
-            newImagePosition = currentImagePosition + (currentIndex * imageWidth);
-        }
-        else{
-            newImagePosition = currentImagePosition - imageWidth;
-        }
-        console.log("sunder");
-        // setInterval(() => {
-        //     dx = currentImagePosition + velocity;
-        //     console.log(dx);
-        //     image.style.left = dx;
-        // },1000);
-        // let a = interval;
-        image.style.left = `${(i * imageWidth)-(imageWidth*currentIndex)}px`;
-        
-    }
-}
-prevBtn.onclick = function () {
-    // console.log(currentIndex,"<<<<<<<<<");
-    if (currentIndex==0){
-        currentIndex=2;
-    }
-    else{
-        currentIndex--;
-    }
-    console.log("<<<<<<<<<",currentIndex);
-    for (let i = 0; i < imageCount; i++) {
-        const image = wrapperImage.children[i];
-        image.style.left = `${(i * imageWidth)-(imageWidth*currentIndex)}px`;
-        console.log(">>>>>>>>>");
-        console.log((i * imageWidth)-(imageWidth*currentIndex));
-    }
-    
-}
-
-
+/**
+ * retrieving div tag from html
+ */
+ var container = document.getElementsByClassName('carousel-container')[0];
+ var wrapperImage =  document.querySelector('.carousel-image-wrapper');
+ wrapperImage.style.left = "0px";
+ 
+ 
+ /**
+  * images variable with value
+  */
+ const imageWidth = 600;
+ const imageCount = wrapperImage.children.length;
+ const wrapperWidth = imageCount * imageWidth;
+ 
+ 
+ 
+ /**
+  * Create elements
+  */
+ const controller = document.createElement('div');
+ const carouselBtnContainer = document.createElement('div');
+ const carouselDotCotainer = document.createElement('div');
+ const nextBtn = document.createElement('button')
+ const prevBtn = document.createElement('button')
+ 
+ controller.classList.add('controller');
+ carouselBtnContainer.classList.add('carouselBtnContainer');
+ carouselDotCotainer.classList.add('carouselDotCotainer');
+ nextBtn.classList.add('carouselBtnContainer-nextBtn');
+ prevBtn.classList.add('carouselBtnContainer-prevBtn');
+ 
+ container.appendChild(controller);
+ controller.appendChild(carouselBtnContainer)
+ controller.appendChild(carouselDotCotainer);
+ carouselBtnContainer.appendChild(prevBtn);
+ carouselBtnContainer.appendChild(nextBtn);
+ 
+ for(let i=0; i<imageCount; i++){
+     var carouselDots = document.createElement('div');
+     carouselDots.classList.add(`carouselDotCotainer-dots${i}`);
+     carouselDotCotainer.appendChild(carouselDots);
+ }
+ 
+ nextBtn.innerHTML="&rangd;";
+ prevBtn.innerHTML="&langd;";
+ 
+ 
+ 
+ 
+ /**
+  * Create horizontal alignment of images using loop
+  */
+ for (let i = 0; i < imageCount; i++) {
+     const image = wrapperImage.children[i];
+     image.style.left = `${i * imageWidth}px`;
+ }
+ 
+ 
+ 
+ 
+ /**
+  * Assigning variable
+  */
+ let currentIndex=0;
+ let speed = 20;
+ let currentWrapperPosition;
+ let newWrapperPosition;
+ let dx=0;
+ let sign = 1;
+ 
+ 
+ 
+ 
+ 
+ /**
+  * function for next-animation
+  * @param {number} currentPosition - currentWrapperPosition
+  * @param {number} finalPosition - newWrapperPosition
+  * @param {number} sign - signvalue
+  */
+ function nextAnimation(currentPosition, finalPosition,sign){
+     dx=currentPosition;
+     let interval = setInterval(() => { 
+                         wrapperImage.style.left = dx +"px";
+                         if (dx==finalPosition){
+                             clearInterval(interval);
+                         }
+                         dx=dx-speed*sign;
+                     },5); 
+     // while(dx!= finalPosition){
+     //     wrapperImage.style.left = dx +"px";
+     //     dx=dx-speed*sign;
+     // }
+     // wrapperImage.style.left = dx +"px";
+ 
+ }
+ 
+ 
+ /**
+  * function for prev-animation
+  * @param {number} currentPosition - currentWrapperPosition
+  * @param {number} finalPosition - newWrapperPosition
+  * @param {number} sign - signvalue
+  */
+ function prevAnimation(currentPosition, finalPosition,sign){ 
+     dx=currentPosition;
+     let interval = setInterval(() => { 
+                         wrapperImage.style.left = dx +"px";
+                         if (dx==finalPosition){
+                             clearInterval(interval);
+                         }
+                         dx=dx+speed*sign;
+                     },5); 
+ 
+ }
+ 
+ 
+ /**
+  * event handler for nextButtton
+  */
+ nextBtn.onclick = function () {
+     currentWrapperPosition = parseInt(wrapperImage.style.left);
+     if(currentIndex == 2){
+         currentIndex = 0;
+         sign=-1;
+         
+     }
+     else{
+         currentIndex++;
+         sign = 1;
+     }
+     newWrapperPosition = -(currentIndex*imageWidth)
+     console.log(sign);
+     nextAnimation(currentWrapperPosition,newWrapperPosition,sign);
+ 
+ }
+ 
+ /**
+  * event handler for previousButtton
+  */
+ prevBtn.onclick = function () {
+     currentWrapperPosition = parseInt(wrapperImage.style.left);
+     if(currentIndex == 0){
+         currentIndex = 2;
+         sign = -1;
+     }
+     else{      
+         currentIndex--;
+         sign = 1;
+     }
+     newWrapperPosition = -(currentIndex*imageWidth);
+     prevAnimation(currentWrapperPosition,newWrapperPosition,sign);
+ 
+ }
+ 
+ 
+ 
+ /**
+  * event handler for  3-dots of carousel
+  * currentIndex is global variable
+  */
+ carouselDotCotainer.children[0].addEventListener('click', function (event) {
+     let dotIndex = 0;
+     currentWrapperPosition = parseInt(wrapperImage.style.left);
+     newWrapperPosition = -(dotIndex * imageWidth);
+     if (currentIndex == dotIndex){
+         //pass
+     }
+     else{
+         sign=1;
+         prevAnimation(currentWrapperPosition,newWrapperPosition,sign);
+     }
+     currentIndex=dotIndex;
+ 
+ });
+ 
+ carouselDotCotainer.children[1].addEventListener('click', function (event) {
+     let dotIndex = 1;
+     currentWrapperPosition = parseInt(wrapperImage.style.left);
+     newWrapperPosition = -(dotIndex * imageWidth)
+     if (currentIndex == dotIndex){
+         //pass
+     }
+     else if(currentIndex>dotIndex){
+         sign=1;
+         prevAnimation(currentWrapperPosition,newWrapperPosition,sign);
+     }
+     else {
+         sign=1;
+         nextAnimation(currentWrapperPosition,newWrapperPosition,sign);
+     }
+     currentIndex=dotIndex;
+ });
+ 
+ 
+ carouselDotCotainer.children[2].addEventListener('click', function (event) {
+     let dotIndex = 2;
+     currentWrapperPosition = parseInt(wrapperImage.style.left);
+     newWrapperPosition = -(dotIndex * imageWidth)
+     if (currentIndex == dotIndex){
+         //pass
+     }
+     else{
+         sign=1;
+         nextAnimation(currentWrapperPosition,newWrapperPosition,sign);
+     }
+     currentIndex=dotIndex;
+ });
+ 
+ 
--- JS/Assignment 1-Image Carousel/src/index.js
+++ JS/Assignment 1-Image Carousel/src/index.js
@@ -1,122 +1,73 @@
-/**
- * retrieving div tag from html
- */
+/*
+
 var container = document.getElementsByClassName('carousel-container')[0];
 var wrapperImage =  document.querySelector('.carousel-image-wrapper');
-wrapperImage.style.left = "0px";
-
 
 const imageWidth = 600;
 const imageCount = wrapperImage.children.length;
-const wrapperWidth = imageCount * imageWidth;
+console.log(imageCount)
 
-
-/**
- * Create elements
- */
-const controller = document.createElement('div');
-const carouselBtnContainer = document.createElement('div');
-const carouselDotCotainer = document.createElement('div');
-const nextBtn = document.createElement('button')
-const prevBtn = document.createElement('button')
-
-controller.classList.add('controller');
-carouselBtnContainer.classList.add('carouselBtnContainer');
-carouselDotCotainer.classList.add('carouselDotCotainer');
-nextBtn.classList.add('carouselBtnContainer-nextBtn');
-prevBtn.classList.add('carouselBtnContainer-prevBtn');
-
-container.appendChild(controller);
-controller.appendChild(carouselBtnContainer)
-controller.appendChild(carouselDotCotainer);
-carouselBtnContainer.appendChild(prevBtn);
-carouselBtnContainer.appendChild(nextBtn);
-
-for(let i=0; i<imageCount; i++){
-    var carouselDots = document.createElement('div');
-    carouselDots.classList.add(`carouselDotCotainer-dots${i}`);
-    carouselDotCotainer.appendChild(carouselDots);
-    // carouselDots.innerHTML="&FilledSmallSquare;";
-}
-
-nextBtn.innerHTML="&rangd;";
-prevBtn.innerHTML="&langd;";
-
-
-/**
- * Create horizontal alignment of images using loop
- */
+// images.style.width = `${imageCount * imageWidth}px`;
+//loop for horizontal alignment of image
 for (let i = 0; i < imageCount; i++) {
     const image = wrapperImage.children[i];
     image.style.left = `${i * imageWidth}px`;
 }
 
+const prevBtn = document.getElementById("prev");
+const nextBtn = document.getElementById("next");
 
 let currentIndex=0;
-let speed = 20;
-let position;
-let currentWrapperPosition;
+let velocity = 10;
+let interval;
 let currentImagePosition;
-let newWrapperPosition;
 let newImagePosition;
-let dx=0;
-let sign = 1;
+let dx;
 console.log("---->>>",currentIndex);
-
-
-
-
-/**
- * event handler for nextButtton
- */
 nextBtn.onclick = function () {
-    if(currentIndex == 2){
-        currentIndex = 0;
-        currentWrapperPosition = parseInt(wrapperImage.style.left);
-        dx=currentWrapperPosition;
-        sign=-1;
+    if (currentIndex==2){
+        currentIndex=0;
     }
     else{
-        sign = 1;
         currentIndex++;
     }
-    newWrapperPosition = - currentIndex*imageWidth;
-
-    let interval = setInterval(() => { 
-                        console.log("sunder");
-                        wrapperImage.style.left = dx +"px";
-                        if (dx==newWrapperPosition){
-                            clearInterval(interval);
-                        }
-                        dx=dx-speed*sign;
-                    },5); 
-            
-    console.log("tamakhus");
-
+    console.log("//////",currentIndex);
+    for (let i = 0; i < imageCount; i++) {
+        const image = wrapperImage.children[i];
+        currentImagePosition = parseInt(image.style.left);
+        if (currentIndex == 2){
+            newImagePosition = currentImagePosition + (currentIndex * imageWidth);
+        }
+        else{
+            newImagePosition = currentImagePosition - imageWidth;
+        }
+        console.log("sunder");
+        // setInterval(() => {
+        //     dx = currentImagePosition + velocity;
+        //     console.log(dx);
+        //     image.style.left = dx;
+        // },1000);
+        // let a = interval;
+        image.style.left = `${(i * imageWidth)-(imageWidth*currentIndex)}px`;
+        
+    }
 }
-
-/**
- * event handler for previousButtton
- */
 prevBtn.onclick = function () {
-    if(currentIndex == 0){
-        currentIndex = 2;
-        currentWrapperPosition = parseInt(wrapperImage.style.left);
-        dx=currentWrapperPosition;
-        sign = -1;
+    // console.log(currentIndex,"<<<<<<<<<");
+    if (currentIndex==0){
+        currentIndex=2;
     }
     else{
-        sign = 1;
         currentIndex--;
     }
-    newWrapperPosition = -currentIndex*imageWidth;
-    let interval = setInterval(() => { 
-                        console.log("sunder");
-                        wrapperImage.style.left = dx +"px";
-                        if (dx==newWrapperPosition){
-                            clearInterval(interval);
-                        }
-                        dx=dx+speed*sign;
-                    },5); 
-
+    console.log("<<<<<<<<<",currentIndex);
+    for (let i = 0; i < imageCount; i++) {
+        const image = wrapperImage.children[i];
+        image.style.left = `${(i * imageWidth)-(imageWidth*currentIndex)}px`;
+        console.log(">>>>>>>>>");
+        console.log((i * imageWidth)-(imageWidth*currentIndex));
+    }
+    
 }
+
+*/
