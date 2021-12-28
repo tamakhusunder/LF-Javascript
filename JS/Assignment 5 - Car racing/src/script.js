af6e7dd033256c8bf1ceb39078fe6c4c23183fff
tamakhusunder <tamakhusunder@gmail.com>
2021-12-29 01:03:34 +0545

    js: assignment5 car upadated

--- JS/Assignment 5 - Car racing/src/script.js
+++ JS/Assignment 5 - Car racing/src/script.js
@@ -1,3 +1,11 @@
+const startMenuDiv = document.querySelector(".start-menu");
+const gamesectionDiv = document.querySelector(".game-section");
+const scoreElement = document.getElementById("scoredigit");
+const highScoreElement = document.getElementById("highscoredigit");
+const gameOverElement = document.querySelector(".exit-menu");
+
+
+
 const car = document.getElementById("car");
 const road = document.querySelector(".road");
 
@@ -12,12 +20,22 @@ const carPaddingWidth = 50;
 const carHeight = 130;
 const carWidth = 100;
 let speed = 5;
+let score = 0;
 
 const laneMap = {
     0: "lane-left",
     1: "lane-middle",
     2: "lane-right"
 };
+
+let initalPage = document.addEventListener("keydown", (event) => {
+    if(event.code == "Enter"){
+        startMenuDiv.style.display = "none";
+        gamesectionDiv.style.display = "block";
+        startAnimation();
+    }
+});
+
 
 let currentCarPosition =
     document.addEventListener("keydown", (event) => {
@@ -35,6 +53,9 @@ let currentCarPosition =
         car.setAttribute("class", `car ${laneMapValue}`);
     });
 
+if(window.localStorage.highscore != undefined){
+    highScoreElement.innerHTML = window.localStorage.getItem('highscore');
+}
 class Obstacle {
     constructor(obsIndex,obsY,obsspeed) {
         this.index = obsIndex;
@@ -44,13 +65,11 @@ class Obstacle {
 
     drawObstacle() {
         this.element = document.createElement("img");
-        // this.element.style.backgroundImage ="img/player.png";
         this.element.src = "img/obstacle.png";
         const laneMapValue = laneMap[this.index];
         this.element.setAttribute("class", `car ${laneMapValue}`);
-        // this.element.style.bottom = "auto";
         this.element.style.top = this.y + "px";
-        // this.element.style.transition = "none";
+        this.element.style.transition = "none";
 
         road.appendChild(this.element);
     }
@@ -61,7 +80,10 @@ class Obstacle {
 
         if (this.y > (laneLength)) {
             this.y = getRandomInt(-150, -800);
+            score++;
+            scoreElement.innerHTML = score;
         }
+
     }
 
     checkCollision() {
@@ -81,10 +103,14 @@ class Obstacle {
             gameCarDim.x + gameCarDim.w > obstacleCarDim.x &&
             gameCarDim.y < obstacleCarDim.y + obstacleCarDim.h &&
             gameCarDim.h + gameCarDim.y > obstacleCarDim.y) {
-            console.log("collision detected!");
-            
+                // console.log("collision detected!");
+                if(window.localStorage.highscore == undefined || window.localStorage.highscore < score){
+                    window.localStorage.setItem("highscore",score);
+                }
+                highScoreElement.innerHTML = window.localStorage.getItem('highscore');
+                return true;
         } else {
-            // no collision
+            return false;
         }
 
     }
@@ -92,7 +118,6 @@ class Obstacle {
 }
 
 const obsArray = [];
-
 for (let i = 0; i < 2; i++) {
     let obsIndex = getRandomInt(0,3);
     let obsY = getRandomInt(-150, -800);
@@ -103,14 +128,27 @@ for (let i = 0; i < 2; i++) {
     obsArray.push(obs);
 }
 
-function animation() {
+function startAnimation() {
+    let collisionFlag = false;
     obsArray.forEach((obs) => {
         obs.moveObstacle();
-        obs.checkCollision();
-    });
+        let flag = obs.checkCollision();
+        if (flag == true){
+            collisionFlag =true;
+        }
 
-    requestAnimationFrame(animation);
+    });
+    if (collisionFlag == true){
+        console.log("gameover")
+        gameOverElement.style.display ="block";
+        return;
+    }
+    requestAnimationFrame(startAnimation);
 }
 
-animation();
+document.addEventListener("keydown", (event) => {
+    if (event.code === "Space") {
+        window.location.reload();
+    }
+});
 
