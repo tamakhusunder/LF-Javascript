f20c255e810854634def96f9574ef5bbc4c7b19a
tamakhusunder <tamakhusunder@gmail.com>
2022-01-09 00:13:30 +0545

    js: finalProject sumoGame added with collision

--- JS/Js-Project(Multiplayer battle game)/core/js/script.js
+++ JS/Js-Project(Multiplayer battle game)/core/js/script.js
@@ -7,6 +7,7 @@ const homeFishBtn = document.querySelector('.home-section').children[3];
 const homePlayBtn = document.querySelector('.home-points-bottom-center');
 const game1 = document.querySelector('.game1');
 const game2 = document.querySelector('.game2');
+const game3 = document.querySelector('.game3');
 
 console.log(homeSoccerBtn)
 console.log(homeChickenBtn)
@@ -25,8 +26,12 @@ homeChickenBtn.addEventListener('click', function (event) {
     console.log("g2")
     let g2 = fn_chicken();
 });
+homeSumoBtn.addEventListener('click', function (event) {
+    console.log("g3")
+    let g3 = fn_sumo();
+});
 
-// let g2 = fn_chicken();
+// let g3 = fn_sumo();
 
 
 
@@ -261,7 +266,7 @@ function  fn_chicken() {
     box1.draw();
     box2.draw();
 
-    // click event for switching 3 stage of screen of soccer game
+    // click event for switching 3 stage of screen of chicken game
     chickenGameTopAreaDiv.addEventListener("click", function (event) {
         console.log("i am click");
         // whistleSound.play();
@@ -319,3 +324,416 @@ function  fn_chicken() {
 
 
 }
+
+function fn_sumo() {
+    console.log("dddd")
+    homePage.style.display = "none";
+    game3.style.display = "block";
+
+
+    const  sumoGameTopAreaDiv = document.querySelector('.game-sumo-top');
+    const  sumoGameBoundaryDiv = document.querySelector('.sumo-border');
+
+
+    const sumoInstruction = document.querySelector('.game-sumo-instruction');
+    const sumoBoard = document.querySelector('.game-sumo-winnerBoard');
+    const sumoBoardline2Div= document.getElementById('sumo-winnerBoard-line2');
+    const sumoBluePointDiv = document.querySelector('.sumo-point-blue');
+    const sumoRedPointDiv = document.querySelector('.sumo-point-red');
+
+
+    const sumoBoundaryWidth = 350;
+    const sumoBoundaryHeight = 350;
+    const sumoGameCenterX = sumoBoundaryWidth/2;    //175
+    const sumoGameCenterY = sumoBoundaryHeight/2;   //175
+
+
+    const state = {
+        current : 0,
+        getReady : 0,
+        gameIn : 1,
+        gameOver : 2
+    }
+    const extraAngle=20;
+    let playerWidth = 70;
+    let playerHeight = 70;
+    let velocity = 0;
+    let walk = 1/8;
+    let initialBlueX = ((sumoBoundaryWidth)/2) - 45;
+    let initialBlueY = (sumoBoundaryHeight)/2 - 140;
+    let initialRedX = initialBlueX;
+    let initialRedY = (sumoBoundaryHeight)/2 + 60;
+    const imgBlue = "core/assets/images/sumo/sumo-blue.png";
+    const imgRed = "core/assets/images/sumo/sumo-red.png";
+
+    // let initialBlueX = ((sumoBoundaryWidth)/2) - playerWidth*5;
+    // let initialBlueY = (sumoBoundaryHeight)/2 - playerHeight/2;
+    // let initialRedX = ((sumoBoundaryWidth)/2) + playerWidth*4;
+    // let initialRedY = initialBlueY;
+    let sumoBluePoint = 0;
+    let sumoRedPoint = 0;
+
+
+    
+
+    class PlayerBlue{
+        constructor(width,height,x,y,name,img,key,direction,velocity,walk){
+            this.width = width;
+            this.height = height;
+            this.radius = this.width/2;
+            this.x = x;
+            this.y = y;
+            this.name = name;
+            this.img = img;
+            this.key = key;
+            this.pushValue = 10;
+            this.direction = direction;
+            this.velocity = velocity;
+            this.walk = walk;
+            this.keyPressed = false;
+            this.angleIt = 0;
+            this.dx = 1;
+            this.dy = 1;
+
+            this.initialX = x;
+            this.initialY = y;
+            console.log(this)
+
+            //player control event with key A and L
+            document.addEventListener('keydown',(event) =>{
+                if(event.code == this.key){
+                    this.keyPressed = true;
+                    
+                }
+            });
+            document.addEventListener('keyup',(event) =>{
+                if(event.code == this.key){
+                    this.keyPressed = false;
+                    
+                }
+            });
+
+        }
+
+        draw() {
+            console.log('sunder')
+            this.playerElement = document.createElement('img');
+            this.playerElement.classList.add(this.name);
+            this.playerElement.src = this.img;
+            this.playerElement.alt = this.name;
+            this.playerElement.style.width = this.width + 'px';
+            this.playerElement.style.height = this.height + 'px';
+            this.playerElement.style.position = 'absolute';
+            this.playerElement.style.left = this.x + 'px';
+            this.playerElement.style.top = this.y + 'px';
+            this.playerElement.style.borderRadius =  50 + '%';
+            
+            sumoGameBoundaryDiv.appendChild(this.playerElement);
+            
+        }
+        update(){
+            if (this.keyPressed == true) this.movePlayer();
+            else this.rotatePlayer();
+            this.checkRingCollision();
+            this.checkPlayerCollisionForPush();
+        }
+
+        checkPlayerCollisionForPush(){
+            let distance = getDistance(this.x,this.y,playerRed.x,playerRed.y);
+            if (distance <= this.radius + playerRed.radius){
+                console.log("collision--blue hit first");
+                this.dx = playerRed.x > this.x ? 1 : -1;
+                this.dy = playerRed.y > this.y ? 1 : -1;
+                playerRed.x = playerRed.x + this.pushValue * this.dx;
+                playerRed.y = playerRed.y + this.pushValue * this.dy;
+                playerRed.playerElement.style.left = playerRed.x  + 'px'; 
+                playerRed.playerElement.style.top = playerRed.y + 'px'; 
+            }
+        }
+
+        checkRingCollision(){
+            if (this.x > sumoBoundaryWidth-this.width){
+                this.x = sumoBoundaryWidth-this.width;
+                this.velocity = 0;
+                this.playerElement.style.left = this.x + "px";
+            }
+            if (this.y > sumoBoundaryHeight-this.height){
+                this.y = sumoBoundaryHeight-this.height;
+                this.velocity = 0;
+                this.playerElement.style.top = this.y + "px";
+            } 
+            if(this.x < 0 - this.width/3){
+                this.x = 0- this.width/3;
+                this.velocity = 0;
+                this.playerElement.style.left = this.x + "px";
+            }
+            if(this.y < 0 - this.width/3){
+                this.y = 0 - this.width/3;
+                this.velocity = 0;
+                this.playerElement.style.top = this.y + "px";
+            }    
+        }
+
+        rotatePlayer(){
+            if((this.angleIt+1)%(360+2) == 0){
+                this.angleIt=0;
+            }
+            this.angle = this.angleIt;
+            this.angleIt++;
+            this.playerElement.style.transform=`rotate(${this.angle}deg)`;
+        }
+
+        //move player in all direction
+        movePlayer(){
+            if (this.keyPressed == true){
+                //control the speed/velocity of player(donot go morethan 5 velocity)
+                if(this.velocity > 5) this.velocity = 3;
+                //face direction movement
+                if ((this.angle >= 360-extraAngle && this.angle <= 360) || (this.angle>=0 && this.angle<=0+extraAngle)){
+                    this.velocity += this.walk;
+                    this.x += this.velocity;
+                    this.playerElement.style.left = this.x + 'px';
+                }
+                else if (this.angle > extraAngle && this.angle < 90-extraAngle){
+                    this.velocity += this.walk;
+                    this.x += this.velocity;
+                    this.y += this.velocity;
+
+                    this.playerElement.style.left = this.x + 'px';
+                    this.playerElement.style.top = this.y + 'px';
+                }
+                else if (this.angle >= 90-extraAngle && this.angle <= 90+extraAngle){
+                    this.velocity += this.walk;
+                    this.y += this.velocity;
+                    this.playerElement.style.top = this.y + 'px';
+                }
+                else if (this.angle > 90+extraAngle && this.angle < 180-extraAngle){
+                    this.velocity += this.walk;
+                    this.x -= this.velocity;
+                    this.y += this.velocity;
+                    this.playerElement.style.left = this.x + 'px';
+                    this.playerElement.style.top = this.y + 'px';
+                }
+                else if (this.angle >= 180-extraAngle && this.angle <= 180+extraAngle){
+                    this.velocity += this.walk;
+                    this.x -= this.velocity;
+                    this.playerElement.style.left = this.x + 'px';
+                }
+                else if (this.angle > 180+extraAngle && this.angle < 270-extraAngle){
+                    this.velocity += this.walk;
+                    this.x -= this.velocity;
+                    this.y -= this.velocity;
+                    this.playerElement.style.left = this.x + 'px';
+                    this.playerElement.style.top = this.y + 'px';
+                }
+                else if (this.angle >= 270-extraAngle && this.angle <= 270+extraAngle){
+                    this.velocity += this.walk;
+                    this.y -= this.velocity;
+                    this.playerElement.style.top = this.y + 'px';
+                }
+                else if (this.angle > 270+extraAngle && this.angle < 360-extraAngle){
+                    this.velocity += this.walk;
+                    this.x += this.velocity;
+                    this.y -= this.velocity;
+                    this.playerElement.style.left = this.x + 'px';
+                    this.playerElement.style.top = this.y + 'px';
+                }
+            }
+        }
+
+        reset(){
+            this.x = this.initialX;
+            this.y = this.initialY;
+            this.playerElement.style.left = this.x + "px";
+            this.playerElement.style.top = this.y + "px";
+        }
+    }
+
+    class PlayerRed{
+        constructor(width,height,x,y,name,img,key,direction,velocity,walk){
+            this.width = width;
+            this.height = height;
+            this.radius = this.width/2;
+            this.x = x;
+            this.y = y;
+            this.name = name;
+            this.img = img;
+            this.key = key;
+            this.pushValue = 10;
+            this.direction = direction;
+            this.velocity = velocity;
+            this.walk = walk;
+            this.keyPressed = false;
+            this.angleIt = 0;
+
+            this.initialX = x;
+            this.initialY = y;
+            console.log(this)
+
+            //player control event with key A and L
+            document.addEventListener('keydown',(event) =>{
+                if(event.code == this.key){
+                    this.keyPressed = true;
+                    
+                }
+            });
+            document.addEventListener('keyup',(event) =>{
+                if(event.code == this.key){
+                    this.keyPressed = false;
+                    
+                }
+            });
+
+        }
+
+        draw() {
+            console.log('sunder')
+            this.playerElement = document.createElement('img');
+            this.playerElement.classList.add(this.name);
+            this.playerElement.src = this.img;
+            this.playerElement.alt = this.name;
+            this.playerElement.style.width = this.width + 'px';
+            this.playerElement.style.height = this.height + 'px';
+            this.playerElement.style.position = 'absolute';
+            this.playerElement.style.left = this.x + 'px';
+            this.playerElement.style.top = this.y + 'px';
+            this.playerElement.style.borderRadius =  50 + '%';
+            
+            sumoGameBoundaryDiv.appendChild(this.playerElement);
+            
+        }
+        update(){
+            if (this.keyPressed == true) this.movePlayer();
+            else this.rotatePlayer();
+            this.checkRingCollision();
+            this.checkPlayerCollision();
+        }
+
+        checkPlayerCollision(){
+            let distance = getDistance(this.x,this.y,playerBlue.x,playerBlue.y);
+            if (distance <= this.radius + playerBlue.radius){
+                console.log("collision-red hit");
+                this.dx = playerBlue.x > this.x ? 1 : -1;
+                this.dy = playerBlue.y > this.y ? 1 : -1;
+                playerBlue.x = playerBlue.x + this.pushValue * this.dx;
+                playerBlue.y = playerRed.y + this.pushValue * this.dy;
+                playerBlue.playerElement.style.left = playerBlue.x  + 'px'; 
+                playerBlue.playerElement.style.top = playerBlue.y + 'px'; 
+            }
+        }
+
+        checkRingCollision(){
+            if (this.x > sumoBoundaryWidth-this.width){
+                this.x = sumoBoundaryWidth-this.width;
+                this.velocity = 0;
+                this.playerElement.style.left = this.x + "px";
+            }
+            if (this.y > sumoBoundaryHeight-this.height){
+                this.y = sumoBoundaryHeight-this.height;
+                this.velocity = 0;
+                this.playerElement.style.top = this.y + "px";
+            } 
+            if(this.x < 0 - this.width/3){
+                this.x = 0- this.width/3;
+                this.velocity = 0;
+                this.playerElement.style.left = this.x + "px";
+            }
+            if(this.y < 0 - this.width/3){
+                this.y = 0 - this.width/3;
+                this.velocity = 0;
+                this.playerElement.style.top = this.y + "px";
+            }    
+        }
+    
+
+        rotatePlayer(){
+            if((this.angleIt+1)%(360+2) == 0){
+                this.angleIt=0;
+            }
+            this.angle = this.angleIt;
+            this.angleIt++;
+            this.playerElement.style.transform=`rotate(${this.angle}deg)`;
+        }
+
+        //move player in all direction
+        movePlayer(){
+            if (this.keyPressed == true){
+                //control the speed/velocity of player(donot go morethan 5 velocity)
+                if(this.velocity > 5) this.velocity = 3;
+                //face direction movement
+                if ((this.angle >= 360-extraAngle && this.angle <= 360) || (this.angle>=0 && this.angle<=0+extraAngle)){
+                    this.velocity += this.walk;
+                    this.x += this.velocity;
+                    this.playerElement.style.left = this.x + 'px';
+                }
+                else if (this.angle > extraAngle && this.angle < 90-extraAngle){
+                    this.velocity += this.walk;
+                    this.x += this.velocity;
+                    this.y += this.velocity;
+
+                    this.playerElement.style.left = this.x + 'px';
+                    this.playerElement.style.top = this.y + 'px';
+                }
+                else if (this.angle >= 90-extraAngle && this.angle <= 90+extraAngle){
+                    this.velocity += this.walk;
+                    this.y += this.velocity;
+                    this.playerElement.style.top = this.y + 'px';
+                }
+                else if (this.angle > 90+extraAngle && this.angle < 180-extraAngle){
+                    this.velocity += this.walk;
+                    this.x -= this.velocity;
+                    this.y += this.velocity;
+                    this.playerElement.style.left = this.x + 'px';
+                    this.playerElement.style.top = this.y + 'px';
+                }
+                else if (this.angle >= 180-extraAngle && this.angle <= 180+extraAngle){
+                    this.velocity += this.walk;
+                    this.x -= this.velocity;
+                    this.playerElement.style.left = this.x + 'px';
+                }
+                else if (this.angle > 180+extraAngle && this.angle < 270-extraAngle){
+                    this.velocity += this.walk;
+                    this.x -= this.velocity;
+                    this.y -= this.velocity;
+                    this.playerElement.style.left = this.x + 'px';
+                    this.playerElement.style.top = this.y + 'px';
+                }
+                else if (this.angle >= 270-extraAngle && this.angle <= 270+extraAngle){
+                    this.velocity += this.walk;
+                    this.y -= this.velocity;
+                    this.playerElement.style.top = this.y + 'px';
+                }
+                else if (this.angle > 270+extraAngle && this.angle < 360-extraAngle){
+                    this.velocity += this.walk;
+                    this.x += this.velocity;
+                    this.y -= this.velocity;
+                    this.playerElement.style.left = this.x + 'px';
+                    this.playerElement.style.top = this.y + 'px';
+                }
+            }
+        }
+
+        reset(){
+            this.x = this.initialX;
+            this.y = this.initialY;
+            this.playerElement.style.left = this.x + "px";
+            this.playerElement.style.top = this.y + "px";
+        }
+    }
+
+
+    
+    let playerBlue = new PlayerBlue(playerWidth,playerHeight,initialBlueX,initialBlueY,'sumoBluePlayer',imgBlue,'KeyA',1,velocity,walk);
+    let playerRed = new PlayerRed(playerWidth,playerHeight,initialRedX,initialRedY,'sumoRedPlayer',imgRed,'KeyL',-1,velocity,walk);
+    playerBlue.draw();
+    playerRed.draw();
+
+    function animation(){
+        playerBlue.update();
+        playerRed.update();
+        requestAnimationFrame(animation);
+    }
+
+    animation();
+}
+
