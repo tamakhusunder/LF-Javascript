11dff91fd6184181544d11edc57c055e6c352537
tamakhusunder <tamakhusunder@gmail.com>
2022-01-13 15:56:03 +0545

    js: FinalProject sumo game refined

--- JS/Js-Project(Multiplayer battle game)/core/js/sumo.js
+++ JS/Js-Project(Multiplayer battle game)/core/js/sumo.js
@@ -1,27 +1,42 @@
-//fn_sumo
+/**
+ *  Whole function for Sumo Wrestling game
+ */
 function fn_sumo() {
     homePage.style.display = "none";
     game3.style.display = "block";
 
 
+    /**
+     * Event handler for back to home page
+     */
+     gotoHomepage = document.getElementById("sumo-gotoHomePage-btn");
+     gotoHomepage.addEventListener('click', function (event) {
+         let homepage = fn_homePage();
+     });
+
+
+     /**
+     * Accessing DOM of sumo game
+     */
     const  sumoGameTopAreaDiv = document.querySelector('.game-sumo-top');
     const  sumoGameBoundaryDiv = document.querySelector('.sumo-border');
     const sumoInstruction = document.querySelector('.game-sumo-instruction');
     const sumoBoard = document.querySelector('.game-sumo-winnerBoard');
     const sumoBoardLine1Div= document.getElementById('sumo-winnerBoard-line1');
     const sumoBoardLine2Div= document.getElementById('sumo-winnerBoard-line2');
+    const sumoBoardLine2DivImg= document.getElementById('sumo-winnerBoard-line2-img');
     const sumoBluePointDiv = document.querySelector('.sumo-point-blue');
     const sumoRedPointDiv = document.querySelector('.sumo-point-red');
+    const sumoEntityControlDiv = document.querySelector(".game-sumo-entity-control");
+    const sumoStateControlDiv = document.getElementById("sumo-state-control-btn");
+    
 
-
+     /**
+     * sumo Game Constants and variable
+     */
     const sumoBoundaryWidth = 350;
     const sumoBoundaryHeight = 350;
-    const state = {
-        current : 0,
-        getReady : 0,
-        gameIn : 1,
-        gameOver : 2
-    }
+    let state;
     const extraAngle=20;
     let playerWidth = 70;
     let playerHeight = 70;
@@ -35,8 +50,15 @@ function fn_sumo() {
     const imgRed = "core/assets/images/sumo/sumo-red.png";
     let sumoBluePoint = 0;
     let sumoRedPoint = 0;
+    let totalGamePoint = 3;
 
 
+    //hoisting instances variable name of class
+    let playerBlue;
+    let playerRed;
+
+
+    /** Class representing for PlayerBlue. */
     class PlayerBlue{
         constructor(width,height,x,y,name,img,key,velocity,walk){
             this.width = width;
@@ -57,6 +79,7 @@ function fn_sumo() {
             this.initialX = x;
             this.initialY = y;
 
+
             //player control event with key A and L
             document.addEventListener('keydown',(event) =>{
                 if(event.code == this.key){
@@ -70,10 +93,10 @@ function fn_sumo() {
                     
                 }
             });
-
         }
 
-        draw() {
+
+        drawPlayerBlue() {
             this.playerElement = document.createElement('img');
             this.playerElement.classList.add(this.name);
             this.playerElement.src = this.img;
@@ -87,6 +110,7 @@ function fn_sumo() {
             sumoGameBoundaryDiv.appendChild(this.playerElement);
         }
         
+
         update(){
             if(state.current === state.gameIn){
                 if (this.keyPressed == true) this.movePlayer();
@@ -96,10 +120,10 @@ function fn_sumo() {
             }
         }
 
+
         checkPlayerCollisionForPush(){
             let distance = getDistance(this.x,this.y,playerRed.x,playerRed.y);
             if (distance <= this.radius + playerRed.radius){
-                // console.log("collision--blue hit first");
                 this.dx = playerRed.x > this.x ? 1 : -1;
                 this.dy = playerRed.y > this.y ? 1 : -1;
                 playerRed.x = playerRed.x + this.pushValue * this.dx;
@@ -109,17 +133,31 @@ function fn_sumo() {
             }
         }
 
+
         checkRingCollision(){
             if (this.x > sumoBoundaryWidth-this.width || this.y > sumoBoundaryHeight-this.height
                 || this.x < 0 - this.width/3 || this.y < 0 - this.width/3){
-                    // console.log("collison boundary blue");
                     sumoRedPoint++;
                     sumoRedPointDiv.innerHTML = sumoRedPoint;
-                    sumoBoardLine1Div.innerHTML = "Blue Out of Ring";
-                    sumoBoardLine2Div.innerHTML = "Red gains the Point";
-                    state.current = state.gameOver;
+                    //condition for end point winner in game(total point to be collect 2 or 3)
+                    if (sumoRedPoint === totalGamePoint){
+                        sumoBoardLine1Div.innerHTML = "Red won the Game";
+                        sumoBoardLine2Div.innerHTML = "";
+                        sumoBoardLine2DivImg.style.display = "block";
+                        sumoRedPoint = 0;
+                        sumoBluePoint = 0;
+                        state.current = state.gameOver;
+                        homeRedPoint++;
+                    }
+                    else{
+                        sumoBoardLine1Div.innerHTML = "Blue Out of Ring";
+                        sumoBoardLine2Div.innerHTML = "Red gains the Point";
+                        sumoBoardLine2DivImg.style.display = "none";
+                        state.current = state.gameOver;
+                    }
                 }
         }
+
 
         rotatePlayer(){
             //reset angle to 0 after 360 degree
@@ -130,6 +168,7 @@ function fn_sumo() {
             this.angleIt++;
             this.playerElement.style.transform=`rotate(${this.angle}deg)`;
         }
+
 
         //move player in all direction
         movePlayer(){
@@ -189,6 +228,7 @@ function fn_sumo() {
             }
         }
 
+
         reset(){
             this.x = this.initialX;
             this.y = this.initialY;
@@ -197,6 +237,8 @@ function fn_sumo() {
         }
     }
 
+
+    /** Class representing for PlayerRed. */
     class PlayerRed{
         constructor(width,height,x,y,name,img,key,velocity,walk){
             this.width = width;
@@ -217,23 +259,22 @@ function fn_sumo() {
             this.initialX = x;
             this.initialY = y;
 
+
             //player control event with key A and L
             document.addEventListener('keydown',(event) =>{
                 if(event.code == this.key){
                     this.keyPressed = true;
-                    
                 }
             });
             document.addEventListener('keyup',(event) =>{
                 if(event.code == this.key){
                     this.keyPressed = false;
-                    
                 }
             });
-
         }
 
-        draw() {
+
+        drawPlayerRed() {
             this.playerElement = document.createElement('img');
             this.playerElement.classList.add(this.name);
             this.playerElement.src = this.img;
@@ -247,6 +288,7 @@ function fn_sumo() {
             sumoGameBoundaryDiv.appendChild(this.playerElement);
         }
 
+
         update(){
             if(state.current === state.gameIn){
                 if (this.keyPressed == true) this.movePlayer();
@@ -256,10 +298,10 @@ function fn_sumo() {
             }
         }
 
+
         checkPlayerCollisionForPush(){
             let distance = getDistance(this.x,this.y,playerBlue.x,playerBlue.y);
             if (distance <= this.radius + playerBlue.radius){
-                // console.log("collision-red first hit");
                 this.dx = playerBlue.x > this.x ? 1 : -1;
                 this.dy = playerBlue.y > this.y ? 1 : -1;
                 playerBlue.x = playerBlue.x + this.pushValue * this.dx;
@@ -269,18 +311,32 @@ function fn_sumo() {
             }
         }
 
+
         checkRingCollision(){
             if (this.x > sumoBoundaryWidth-this.width || this.y > sumoBoundaryHeight-this.height
                 || this.x < 0 - this.width/3 || this.y < 0 - this.width/3){
-                    // console.log("collison boundary red");
                     sumoBluePoint++;
                     sumoBluePointDiv.innerHTML = sumoBluePoint;
-                    sumoBoardLine1Div.innerHTML = "Red Out of Ring";
-                    sumoBoardLine2Div.innerHTML = "Blue gains the Point";
-                    state.current = state.gameOver;
+                    //condition for end point winner in game(total point to be collect 2 or 3)
+                    if (sumoBluePoint === totalGamePoint){
+                        sumoBoardLine1Div.innerHTML = "Blue won the Game";
+                        sumoBoardLine2Div.innerHTML = "";
+                        sumoBoardLine2DivImg.style.display = "block";
+                        sumoRedPoint = 0;
+                        sumoBluePoint = 0;
+                        state.current = state.gameOver;
+                        homeBluePoint++;
+                    }
+                    else{
+                        sumoBoardLine1Div.innerHTML = "Red Out of Ring";
+                        sumoBoardLine2Div.innerHTML = "Blue gains the Point";
+                        sumoBoardLine2DivImg.style.display = "none";
+                        state.current = state.gameOver;
+                    }
                 } 
         }
     
+
         rotatePlayer(){
             //reset angle to 360 after 0 degree
             if (this.angleIt < 0){
@@ -290,6 +346,7 @@ function fn_sumo() {
             this.angleIt--;
             this.playerElement.style.transform=`rotate(${this.angle}deg)`;
         }
+
 
         //move player in all direction
         movePlayer(){
@@ -349,6 +406,7 @@ function fn_sumo() {
             }
         }
 
+
         reset(){
             this.x = this.initialX;
             this.y = this.initialY;
@@ -358,22 +416,45 @@ function fn_sumo() {
     }
 
     
-    let playerBlue = new PlayerBlue(playerWidth,playerHeight,initialBlueX,initialBlueY,'sumoBluePlayer',imgBlue,'KeyA',velocity,walk);
-    let playerRed = new PlayerRed(playerWidth,playerHeight,initialRedX,initialRedY,'sumoRedPlayer',imgRed,'KeyL',velocity,walk);
-    playerBlue.draw();
-    playerRed.draw();
+    /**
+     * state object is host once
+     * draw the instances of class for once only
+     */
+     if (clickHomeSumoOnce === false){
+        state = {
+            current : 0,
+            getReady : 0,
+            gameIn : 1,
+            gameOver : 2
+        }
+    
+
+        playerBlue = new PlayerBlue(playerWidth,playerHeight,initialBlueX,initialBlueY,'sumoBluePlayer',imgBlue,'KeyA',velocity,walk);
+        playerRed = new PlayerRed(playerWidth,playerHeight,initialRedX,initialRedY,'sumoRedPlayer',imgRed,'KeyL',velocity,walk);
+        playerBlue.drawPlayerBlue();
+        playerRed.drawPlayerRed();
+        clickHomeSumoOnce = true;
+    }
+    else{
+       playerBlue.reset();
+       playerRed.reset();
+    }
 
 
     // click event for switching 3 stage of screen of sumo game
-    sumoGameTopAreaDiv.addEventListener("click", function (event) {
+    sumoStateControlDiv.addEventListener("click", function (event) {
         switch(state.current){
             case state.getReady:
                 state.current = state.gameIn;
+                sumoEntityControlDiv.style.display = "none";
                 break;
             case state.gameIn:
                 break;
             case state.gameOver:
                 state.current = state.gameIn;
+                sumoEntityControlDiv.style.display = "none";
+                sumoBluePointDiv.innerHTML = sumoBluePoint;
+                sumoRedPointDiv.innerHTML = sumoRedPoint;
                 playerBlue.reset();
                 playerRed.reset();
                 break;
@@ -383,29 +464,33 @@ function fn_sumo() {
     });
 
 
+    /** Fuction for showing or hiding the Instruction div and Button entity of homepage and restart button */
     function showInstruction() {
         if (state.current === state.getReady){
             sumoInstruction.style.display = "block";
+            sumoEntityControlDiv.style.display = "block";
         }
         else sumoInstruction.style.display = "none";
     }
 
 
+    /** Fuction for showing or hiding the winner div and Button entity of homepage and restart button */
     function showBoard() {
         if (state.current === state.gameOver){
             sumoBoard.style.display = "block";
+            sumoEntityControlDiv.style.display = "block";
         }
         else sumoBoard.style.display = "none";
     }
 
-
-    function animation(){
+    /** Function of loop to show state of game and box animation*/
+    function animationWithChangeState(){
         showInstruction();
         showBoard();
         playerBlue.update();
         playerRed.update();
-        requestAnimationFrame(animation);
+        requestAnimationFrame(animationWithChangeState);
     }
 
-    animation();
+    animationWithChangeState();
 }
